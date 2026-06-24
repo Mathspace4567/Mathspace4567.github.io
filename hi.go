d0af37d14c5728c29fbff60a998712b3a7396fda
Mathspace <harrythesnacker@gmail.com>
2026-06-24 20:22:00 +0800

    Create hi.go

---
+++ hi.go
@@ -0,0 +1,12 @@
+R
+v<html lang="en">
+• <head>...</head>
+• <body data-package-name="mfe-app-shel1" data-package-version="2.0.0" data-mfe-key="mfeAppShell" data-testid="mfeAppShell" data-buildid="27922566039" data-mfe-composer-services="services: live" data-mfe-vertical-navigation-size="large" data-mfe-vertical-navigation-layout="default" class="learner-e xperience desktop">...</body>
+</html>
+body.learne
+Attribute
+data-packa data-packa
+data-mfe-k
+data-testid
+data-buildi
+data-mfe-c
